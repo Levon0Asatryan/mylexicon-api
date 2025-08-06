f9668ff9f11877fd2e943f9b508d2491ca21d550
Levonasatryan1098@gmail.com <levon.asatryan@cloudchipr.com>
2025-08-06 09:16:08 +0400

    initial commit

---
+++ src/main/kotlin/com/example/mylexicon/MyLexiconApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mylexicon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyLexiconApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyLexiconApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mylexicon/MyLexiconApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mylexicon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyLexiconApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
